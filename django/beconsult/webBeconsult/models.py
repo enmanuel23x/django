d08fc7d653f3a44e0cd9c8c2c624bc0c7d0e9e08
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-05-05 12:03:34 -0400

    Update

--- django/beconsult/webBeconsult/models.py
+++ django/beconsult/webBeconsult/models.py
@@ -43,7 +43,7 @@ class JobOffer(models.Model):
         """Unicode representation of JobOffer."""
         return self.Titulo
     def getTags(self):
-        return str(self.Tags_en_Perfil).split("&")
+        return str(self.Tags_en_Perfil).split("\n")
             
 
 class Employees(models.Model):
